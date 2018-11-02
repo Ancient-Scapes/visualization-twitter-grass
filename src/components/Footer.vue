4768767a08b8fd62971e0553748d808c6ad1c371
SugarShootingStar <ryu.8224@gmail.com>
2018-11-02 20:33:54 +0900

    feat:フッターにCopyrightを追加

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="8" 
+        vs-offset="2" class="footer">
+    <small>Copyright © SugarShootingStar All Rights Reserved.</small>
+  </vs-col>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style lang="scss">
+.footer {
+  margin-top: 5%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,32 @@
 <template>
   <div class="home">
-    <UploadCsv v-on:success-upload="drawing"/>
+    <UploadCsv v-on:success-upload="drawingCalendar"/>
     <Heatmap :contribution="contribution" ref="heatmap"/>
+    <Share/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Heatmap from '@/components/Heatmap.vue';
 import UploadCsv from '@/components/UploadCsv.vue';
-
+import Share from '@/components/Share.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'home',
   components: {
-    Heatmap, UploadCsv,
+    Heatmap, UploadCsv, Share, Footer,
   },
   data() {
     return {
       contribution: [],
-    }
+    };
   },
   methods: {
-    drawing: function(contribution){
+    drawingCalendar: function (contribution) {
       this.contribution = contribution;
     },
-  }
+  },
 };
 </script>
