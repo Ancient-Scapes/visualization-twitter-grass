4f38bbf9c473113accb35335f0c745bae8808298
SugarShootingStar <ryu.8224@gmail.com>
2018-11-04 23:06:14 +0900

    feat:結果をダウンロードするボタン追加

--- src/components/Heatmap.vue
+++ src/components/Heatmap.vue
@@ -2,12 +2,15 @@
   <div class="heatmap">
     <calendar-heatmap :values="contribution" :endDate="today" :tooltip-unit="tooltipUnit"
                       class="calendar-heatmap"/>
+    <vs-button color="primary" type="border" icon="save" class="button-download"
+               @click="downloadHeatmap" >ダウンロード</vs-button>
   </div>
 </template>
 
 <script>
 import { CalendarHeatmap } from 'vue-calendar-heatmap';
 import 'vue-calendar-heatmap/dist/vue-calendar-heatmap.css';
+import canvg from 'canvg';
 
 export default {
   name: 'Heatmap',
@@ -23,6 +26,36 @@ export default {
       today: this.$moment(new Date()).format('YYYY-MM-DD'),
     };
   },
+  methods: {
+    downloadHeatmap: function () {
+      const heatmap = document.getElementsByClassName('calendar-heatmap')[0];
+      // ヒートマップが描画されているsvgを画像に変換しダウンロード
+      this.convertSvgToImg(heatmap, this.downloadImg.bind(this));
+    },
+    // svgを画像に変換
+    convertSvgToImg: function (svg, callback) {
+      // canvasを動的に作成
+      const canvas = document.createElement('canvas');
+      canvas.width = svg.width.baseVal.value;
+      canvas.height = svg.height.baseVal.value;
+
+      // svgをcanvasに描画
+      const data = new XMLSerializer().serializeToString(svg);
+      canvg(canvas, data);
+      callback(canvas);
+    },
+    // 画像ダウンロードリンクを作成
+    generateLink: function (fileName, img) {
+      const link = document.createElement('a');
+      link.download = fileName;
+      link.href = img;
+      return link;
+    },
+    downloadImg: function (canvas) {
+      const img = canvas.toDataURL('image/png');
+      this.generateLink('twithub-result.png', img).click();
+    },
+  },
 };
 </script>
 
@@ -32,6 +65,12 @@ export default {
   margin: 0 3% 3% 3%;
   border: 1px solid #e1e4e8;
   padding: 20px;
+}
+
+#canvas-heatmap {
+  font-size: 9px;
+  width: 917px;
+  height: 157px;
 }
 
 // すまほ
